3a5f06e5c482df784a6e864e788af6a546b66e9d
ionutsavin <ionutsavin6@gmail.com>
2024-03-12 19:42:04 +0200

    update

--- Lab3/lab3/src/main/java/org/example/Visitable.java
+++ Lab3/lab3/src/main/java/org/example/Visitable.java
@@ -2,15 +2,15 @@ package org.example;
 import java.time.*;
 import java.util.*;
 public interface Visitable {
-    public Map<LocalDate, TimeInterval> getTimetable();
-    public default LocalTime getOpeningHour(LocalDate date) {
+    Map<LocalDate, TimeInterval> getTimetable();
+    default LocalTime getOpeningHour(LocalDate date) {
         TimeInterval interval = getTimetable().get(date);
         if (interval != null) {
             return interval.getStartTime();
         }
         return null;
     }
-    public default LocalTime getFinishingHour(LocalDate date){
+    default LocalTime getFinishingHour(LocalDate date){
         TimeInterval interval = getTimetable().get(date);
         if (interval != null) {
             return interval.getEndTime();
